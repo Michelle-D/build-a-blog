02916a311c9da50bfd9a9207ea092506be10c856
Michelle Dumler <shels.mail@gmail.com>
2017-02-14 05:12:05 -0600

    fixed templates, still needs to be split into separate pages

--- main.py
+++ main.py
@@ -5,49 +5,45 @@ import jinja2
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape = True)
+jinja_env = jinja2.Environment (loader = jinja2.FileSystemLoader (template_dir),
+                               autoescape = True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
-    def render_str(self, template, **params):
+    def render_str (self, template, **params):
         t = jinja_env.get_template(template)
         return t.render(params)
 
-    def render(self, template, **kw):
+    def render (self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class Content(db.Model):
-    subject = db.StringProperty(required = True)
-    content = db.TextProperty(required = True)
+class Blog(db.Model):  #creates an intity
+    title = db.StringProperty(required = True)
+    blog = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
 
 class MainPage(Handler):
-    def render_blog(self, subject = "", content = "", error = ""):
-        blogs = db.GqlQuery("SELECT * FROM post ORDER BY created DESC LIMIT 5")
-        self.render("blog.html", subject=subject, content=content, error=error, blogs=blogs)
+    def render_front(self, title="", blog="", error=""):
+        blogs = db.GqlQuery("SELECT * FROM Blog ORDER BY created DESC LIMIT 5")
+        self.render("newpost.html", title=title, blog=blog, error=error, blogs=blogs)
 
-    def get(self):
-        self.render_blog()
+    def get (self):
+        self.render_front()
 
-    def post(self):
-        subject = self.request.get("subject")
-        content = self.request.get("content")
+    def post (self):
+        title = self.request.get("title")
+        blog = self.request.get("blog")
 
-        if subject and content:
-            a = Content(subject = subject, content = content)
-            a.put()
+        if title and art:
+            b = Blog(title = title, blog = blog) #creates new instence of blog
+            b.put() #stores new blog object in database
 
             self.redirect("/")
         else:
-            error = "We need both a subject and some content"
-            self.render_blog(subject, content, error)
-
-#class NewPost(Handler):
-#    def get(self):
-#        self.render("newpost.html")
-
+            error = "we need both a title and a blog"
+            self.render_front(title, blog, error)
 
 class ViewPost(webapp2.RequestHandler):
     def get(self, id):
@@ -55,4 +51,5 @@ class ViewPost(webapp2.RequestHandler):
 
 
 app = webapp2.WSGIApplication([('/', MainPage),
+                                #("/newpost", NewPost),
                                 webapp2.Route('/blog/<id:\d+>', ViewPost)], debug =True)
